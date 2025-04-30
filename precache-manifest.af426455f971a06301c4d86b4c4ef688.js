f84cfb8c830e06a4c5b6b505668d4bfac8e1ae80
Ricardo Lopez Hernandez <richard@MacBook-Air-de-Ricardo.local>
2025-04-29 21:30:55 -0600

    Updates

---
+++ precache-manifest.af426455f971a06301c4d86b4c4ef688.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cc006c75030830b4b5de51f9bf265a2",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "c0e2ff2323c56681b6e5",
+    "url": "/website/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "641eb57c02ac320c60d0",
+    "url": "/website/static/css/main.c722c411.chunk.css"
+  },
+  {
+    "revision": "c0e2ff2323c56681b6e5",
+    "url": "/website/static/js/2.5cb0fba8.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/website/static/js/2.5cb0fba8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "641eb57c02ac320c60d0",
+    "url": "/website/static/js/main.5aacdb63.chunk.js"
+  },
+  {
+    "revision": "171dd2907eb41a9b6aec",
+    "url": "/website/static/js/runtime-main.7c57d86c.js"
+  }
+]);
